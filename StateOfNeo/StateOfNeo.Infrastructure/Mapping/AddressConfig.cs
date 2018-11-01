6227b069a5afb0a5f3a6d2cf38c957adc7b59954
Zdravko Georgiev <skinsomnia@gmail.com>
2018-11-01 19:54:07 +0200

    Added addresses listing

---
+++ StateOfNeo/StateOfNeo.Infrastructure/Mapping/AddressConfig.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using StateOfNeo.Common.Extensions;
+using StateOfNeo.Data.Models;
+using StateOfNeo.ViewModels.Address;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StateOfNeo.Infrastructure.Mapping
+{
+    public class AddressConfig
+    {
+        internal static void InitMap(IMapperConfigurationExpression cfg)
+        {
+            cfg.CreateMap<Address, AddressListViewModel>()
+                .ForMember(x => x.Address, opt => opt.MapFrom(x => x.PublicAddress))
+                .ForMember(x => x.Created, opt => opt.MapFrom(x => x.FirstTransactionOn))
+                .ForMember(x => x.Transactions, opt => opt.MapFrom(x => x.OutgoingTransactions.Count()))
+                .ForMember(x => x.LastTransactionTime, opt => opt.MapFrom(x => x.OutgoingTransactions.Select(tr => tr.Transaction.Block.Timestamp).OrderByDescending(ts => ts).FirstOrDefault().ToUnixDate()))
+
+                .ReverseMap();
+
+        }
+    }
+}
---
+++ StateOfNeo/StateOfNeo.ViewModels/Address/AddressListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateOfNeo.ViewModels.Address
+{
+    public class AddressListViewModel
+    {
+        public string Address { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public int Transactions { get; set; }
+
+        public DateTime LastTransactionTime { get; set; }
+
+        public IEnumerable<AddressAssetViewModel> Assets { get; set; }
+    }
+}
