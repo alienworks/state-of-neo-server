e005ce8a5e834bae301fcc86e37ff93243ac2f6e
Zdravko Georgiev <skinsomnia@gmail.com>
2018-11-05 18:53:26 +0200

    Node service filters nodes by net setting

--- StateOfNeo/StateOfNeo.Services/Node/NodeService.cs
+++ StateOfNeo/StateOfNeo.Services/Node/NodeService.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper.QueryableExtensions;
+using Microsoft.Extensions.Options;
+using StateOfNeo.Common;
 using StateOfNeo.Data;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,16 +11,18 @@ namespace StateOfNeo.Services
     public class NodeService : INodeService
     {
         private readonly StateOfNeoContext ctx;
+        private readonly NetSettings netSettings;
 
-        public NodeService(StateOfNeoContext ctx)
+        public NodeService(StateOfNeoContext ctx, IOptions<NetSettings> netSettings)
         {
             this.ctx = ctx;
+            this.netSettings = netSettings.Value;
         }
 
         public async Task<IPagedList<T>> GetPage<T>(int page = 1, int pageSize = 10)
         {
             var result = await this.ctx.Nodes
-                .Where(x => x.SuccessUrl != null)
+                .Where(x => x.SuccessUrl != null && x.Net == this.netSettings.Net)
                 .ProjectTo<T>()
                 .ToPagedListAsync(page, pageSize);
 
