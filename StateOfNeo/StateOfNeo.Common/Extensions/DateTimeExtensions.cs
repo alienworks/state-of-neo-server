697ec83c349df72e63cbe084fb714e081a807d62
Zdravko Georgiev <skinsomnia@gmail.com>
2018-11-13 19:08:10 +0200

    DateTme extensions added

---
+++ StateOfNeo/StateOfNeo.Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateOfNeo.Common.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static long ToUnixTimestamp(this DateTime date) =>
+            (long)date.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+    }
+}
