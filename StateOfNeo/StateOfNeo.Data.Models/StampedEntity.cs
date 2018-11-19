d2500b9bc657c71976c3d833192fddda5c0245b3
Zdravko Georgiev <skinsomnia@gmail.com>
2018-11-19 20:51:59 +0200

    Added monthly, daily and hourly timestamps to StampedEntity

--- StateOfNeo/StateOfNeo.Data.Models/StampedEntity.cs
+++ StateOfNeo/StateOfNeo.Data.Models/StampedEntity.cs
@@ -3,5 +3,11 @@
     public abstract class StampedEntity : BaseEntity
     {
         public long Timestamp { get; set; }
+
+        public long MonthlyStamp { get; set; }
+
+        public long DailyStamp { get; set; }
+
+        public long HourlyStamp { get; set; }
     }
 }
