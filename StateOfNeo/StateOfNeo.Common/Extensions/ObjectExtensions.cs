6980cab278bbaca2d023e9338288113e6f1d6763
Fortunesky <fortuneskylin@hotmail.com>
2019-12-28 19:53:08 +0800

    adding DeepClone method;

--- StateOfNeo/StateOfNeo.Common/Extensions/ObjectExtensions.cs
+++ StateOfNeo/StateOfNeo.Common/Extensions/ObjectExtensions.cs
@@ -19,5 +19,17 @@ namespace StateOfNeo.Common
             FieldInfo field = type.GetField(fieldName, bindFlags);
             return (T)field.GetValue(instance);
         }
+
+        public static T DeepClone<T>(this T obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, obj);
+                ms.Position = 0;
+
+                return (T)formatter.Deserialize(ms);
+            }
+        }
     }
 }
