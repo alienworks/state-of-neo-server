5d5679b53f48a52c008a86f138cdb0cdc2ae32b6
Omega9i <omega9i@alienworks.io>
2020-04-30 17:46:52 +0800

    refactor redisHelper;

---
+++ StateOfNeo/StateOfNeo.Data.Caching/Redis/RHHash.cs
@@ -0,0 +1,75 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateOfNeo.Data.Caching.Redis
+{
+    public partial class RedisHelper
+    {
+        #region Sync
+
+        public bool HashExists(string key, string dataKey)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.HashExists(key, dataKey));
+        }
+
+        public bool HashSet<T>(string key, string dataKey, T t)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db =>
+            {
+                string json = ConvertJson(t);
+                return db.HashSet(key, dataKey, json);
+            });
+        }
+
+        public bool HashDelete(string key, string dataKey)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.HashDelete(key, dataKey));
+        }
+
+        public long HashDelete(string key, List<RedisValue> dataKeys)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.HashDelete(key, dataKeys.ToArray()));
+        }
+
+        public T HashGet<T>(string key, string dataKey)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db =>
+            {
+                string value = db.HashGet(key, dataKey);
+                return ConvertObj<T>(value);
+            });
+        }
+
+        public double HashIncrement(string key, string dataKey, double val = 1)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.HashIncrement(key, dataKey, val));
+        }
+
+        public double HashDecrement(string key, string dataKey, double val = 1)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.HashDecrement(key, dataKey, val));
+        }
+
+        public List<T> HashKeys<T>(string key)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db =>
+            {
+                RedisValue[] values = db.HashKeys(key);
+                return ConvetList<T>(values);
+            });
+        }
+
+        #endregion Hash
+    }
+}
---
+++ StateOfNeo/StateOfNeo.Data.Caching/Redis/RHString.cs
@@ -0,0 +1,113 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateOfNeo.Data.Caching.Redis
+{
+    public partial class RedisHelper
+    {
+        #region Sync
+
+        /// <summary>
+        /// Set string value
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <param name="value">value</param>
+        /// <param name="expiration">expiration time</param>
+        /// <returns></returns>
+        public bool StringSet(string key, string value, TimeSpan? expiration = default(TimeSpan?))
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.StringSet(key, value, expiration));
+        }
+
+        /// <summary>
+        /// Set multiple keyvalues
+        /// </summary>
+        /// <param name="keyValues">list of KeyVlues</param>
+        /// <returns>True if the keys were set, else False</returns>
+        public bool StringSet(List<KeyValuePair<RedisKey, RedisValue>> keyValues)
+        {
+            List<KeyValuePair<RedisKey, RedisValue>> newkeyValues =
+                keyValues.Select(p => new KeyValuePair<RedisKey, RedisValue>(AddSysCustomKey(p.Key), p.Value)).ToList();
+            return Do(db => db.StringSet(newkeyValues.ToArray()));
+        }
+
+        /// <summary>
+        /// Set object value
+        /// </summary>
+        /// <typeparam name="T">object value</typeparam>
+        /// <param name="key"></param>
+        /// <param name="obj"></param>
+        /// <param name="expiration">expiration time</param>
+        /// <returns></returns>
+        public bool StringSet<T>(string key, T obj, TimeSpan? expiration = default(TimeSpan?))
+        {
+            key = AddSysCustomKey(key);
+            string json = ConvertJson(obj);
+            return Do(db => db.StringSet(key, json, expiration));
+        }
+
+        /// <summary>
+        /// Get string key
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <returns>string value</returns>
+        public string StringGet(string key)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.StringGet(key));
+        }
+
+        /// <summary>
+        /// Get multiple redis values
+        /// </summary>
+        /// <param name="listKey">list of string keys</param>
+        /// <returns>list of redis values</returns>
+        public RedisValue[] StringGet(List<string> listKey)
+        {
+            List<string> newKeys = listKey.Select(AddSysCustomKey).ToList();
+            return Do(db => db.StringGet(ConvertRedisKeys(newKeys)));
+        }
+
+        /// <summary>
+        /// Get object key
+        /// </summary>
+        /// <typeparam name="T">object type</typeparam>
+        /// <param name="key">string key</param>
+        /// <returns>object value</returns>
+        public T StringGet<T>(string key)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => ConvertObj<T>(db.StringGet(key)));
+        }
+
+        /// <summary>
+        /// Increment value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="val">The amount to increment by (defaults to 1 and can be negative)</param>
+        /// <returns>The value of key after the increment</returns>
+        public double StringIncrement(string key, double val = 1)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.StringIncrement(key, val));
+        }
+
+        /// <summary>
+        /// Decrement value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="val">The amount to decrement by (defaults to 1 and can be negative)</param>
+        /// <returns>The value of key after the decrement</returns>
+        public double StringDecrement(string key, double val = 1)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.StringDecrement(key, val));
+        }
+
+        #endregion Sync
+    }
+}
